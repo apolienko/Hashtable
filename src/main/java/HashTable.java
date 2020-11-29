d62aa034c47776bb14f9ec265150362aea56e122
elik <elik@example.com>
2020-11-30 01:23:18 +0500

    Start

---
+++ src/main/java/HashTable.java
@@ -0,0 +1,102 @@
+import java.util.Collection;
+import java.util.Map;
+import java.util.Set;
+import java.util.function.BiConsumer;
+import java.util.function.BiFunction;
+import java.util.function.Function;
+
+public class HashTable implements Map {
+
+
+    public int size() {
+        return 0;
+    }
+
+    public boolean isEmpty() {
+        return false;
+    }
+
+    public boolean containsKey(Object key) {
+        return false;
+    }
+
+    public boolean containsValue(Object value) {
+        return false;
+    }
+
+    public Object get(Object key) {
+        return null;
+    }
+
+    public Object put(Object key, Object value) {
+        return null;
+    }
+
+    public Object remove(Object key) {
+        return null;
+    }
+
+    public void putAll(Map m) {
+
+    }
+
+    public void clear() {
+
+    }
+
+    public Set keySet() {
+        return null;
+    }
+
+    public Collection values() {
+        return null;
+    }
+
+    public Set<Entry> entrySet() {
+        return null;
+    }
+
+    public Object getOrDefault(Object key, Object defaultValue) {
+        return null;
+    }
+
+    public void forEach(BiConsumer action) {
+
+    }
+
+    public void replaceAll(BiFunction function) {
+
+    }
+
+    public Object putIfAbsent(Object key, Object value) {
+        return null;
+    }
+
+    public boolean remove(Object key, Object value) {
+        return false;
+    }
+
+    public boolean replace(Object key, Object oldValue, Object newValue) {
+        return false;
+    }
+
+    public Object replace(Object key, Object value) {
+        return null;
+    }
+
+    public Object computeIfAbsent(Object key, Function mappingFunction) {
+        return null;
+    }
+
+    public Object computeIfPresent(Object key, BiFunction remappingFunction) {
+        return null;
+    }
+
+    public Object compute(Object key, BiFunction remappingFunction) {
+        return null;
+    }
+
+    public Object merge(Object key, Object value, BiFunction remappingFunction) {
+        return null;
+    }
+}

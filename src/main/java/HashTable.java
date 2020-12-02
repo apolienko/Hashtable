ba820404d51088ebb984c8cca5a3a262e0561808
elik <elik@example.com>
2020-12-03 00:58:23 +0500

    hash functions and some methods

--- src/main/java/HashTable.java
+++ src/main/java/HashTable.java
@@ -1,29 +1,78 @@
-import java.util.Collection;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 import java.util.function.BiConsumer;
 import java.util.function.BiFunction;
 import java.util.function.Function;
 
 public class HashTable implements Map {
 
+    private final int HASH_CONST = 47;
+    private int size = 0;
+    private int bufferSize = 10;
+    private Cell[] hashTable;
+    private boolean[] deletedCells;
+
+    public HashTable() {
+        hashTable = new Cell[bufferSize];
+        deletedCells = new boolean[bufferSize];
+    }
+
+    public HashTable(int bufferSize) {
+        this.bufferSize = bufferSize;
+        hashTable = new Cell[bufferSize];
+        deletedCells = new boolean[bufferSize];
+    }
 
     public int size() {
-        return 0;
+        return size;
     }
 
     public boolean isEmpty() {
+        return size == 0;
+    }
+
+    private boolean contains(Object key) {
+        int hash1 = hash1(key);
+        int hash2 = hash2(key);
+        int n = -1;
+        while (n != bufferSize - 1) {
+            n++;
+            int index = (hash1 + n * hash2) % (bufferSize - 1);
+            Cell cell = hashTable[index];
+            if (cell != null && cell.getKey().equals(key)) {
+                if (!deletedCells[index]) {
+                    return false;
+                } else {
+                    return true;
+                }
+            }
+        }
         return false;
     }
 
+    @Override
     public boolean containsKey(Object key) {
-        return false;
+        if (key == null) {
+            throw new NullPointerException();
+        }
+        return contains(key);
     }
 
+    @Override
     public boolean containsValue(Object value) {
+        if (value == null) {
+            throw new NullPointerException();
+        }
+        for (int i = 0; i < bufferSize; i++) {
+            if (deletedCells[i]) {
+                if (value.equals(hashTable[i].getValue())) {
+                    return true;
+                }
+            }
+        }
         return false;
     }
 
+    @Override
     public Object get(Object key) {
         return null;
     }
@@ -98,5 +147,17 @@ public class HashTable implements Map {
 
     public Object merge(Object key, Object value, BiFunction remappingFunction) {
         return null;
+    }
+
+    private int hash1(Object key) {
+        return (key.hashCode() * HASH_CONST)  % bufferSize;
+    }
+
+    private int hash2(Object key) {
+        int hash = (key.hashCode() * HASH_CONST) % (bufferSize - 1);
+        if (hash % 2 == 0) {
+            ++hash;
+        }
+        return hash;
     }
 }

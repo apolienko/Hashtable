2944cc0cc80ba13dfcf01511dda7bb8f9a509605
elik <elik@example.com>
2020-12-09 23:54:45 +0500

    Cell to static nested class

--- src/main/java/HashTable.java
+++ src/main/java/HashTable.java
@@ -568,4 +568,53 @@ public class HashTable<K, V> implements Map<K, V> {
         }
     }
 
+    private static class Cell<K, V> implements Map.Entry<K, V> {
+
+        private K key;
+        private V value;
+
+        public Cell(K key, V value) {
+            this.key = key;
+            this.value = value;
+        }
+
+        @Override
+        public K getKey() {
+            return key;
+        }
+
+        @Override
+        public V getValue() {
+            return value;
+        }
+
+        @Override
+        public V setValue(V value) {
+            this.value = value;
+            return value;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            Cell<?, ?> cell = (Cell<?, ?>) o;
+            return Objects.equals(key, cell.key) &&
+                    Objects.equals(value, cell.value);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(key, value);
+        }
+
+        @Override
+        public String toString() {
+            return "Cell{" +
+                    "key=" + key +
+                    ", value=" + value +
+                    '}';
+        }
+    }
+
 }
